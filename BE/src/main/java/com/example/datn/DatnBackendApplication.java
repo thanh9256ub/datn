7ba6c3d0f4305a8ff6af88ad5a0a48059b418d50
thanh9256ub <thanh9256ub@gmail.com>
2025-01-22 11:12:24 +0700

    ffdg
    fghfg

---
+++ BE/src/main/java/com/example/datn/DatnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.datn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatnBackendApplication.class, args);
+	}
+
+}
