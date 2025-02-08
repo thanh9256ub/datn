706f358b5441fc6d0b228ea2d330354daf32ee9d
longthanh21 <145737956+longthanh21@users.noreply.github.com>
2025-02-08 15:11:18 +0700

    long commitcommit

--- BE/src/main/java/com/example/datn/repository/ThuongHieuRepository.java
+++ BE/src/main/java/com/example/datn/repository/ThuongHieuRepository.java
@@ -1,9 +1,22 @@
 package com.example.datn.repository;
 
+import com.example.datn.dto.response.ThuongHieuResponse;
 import com.example.datn.entity.ThuongHieu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ThuongHieuRepository extends JpaRepository<ThuongHieu,Integer> {
+    @Query("""
+        SELECT new com.example.datn.dto.response.ThuongHieuResponse(
+            th.id,
+            th.maThuongHieu,
+            th.tenThuongHieu,
+            th.moTa
+        ) FROM ThuongHieu th
+    """)
+    List<ThuongHieuResponse> getAllResponse();
 }
