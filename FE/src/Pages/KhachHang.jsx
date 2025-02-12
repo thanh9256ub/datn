ada45978d9b2db3526298af16f3e872029c1e3a8
tuan45734 <tuan45734>
2025-02-13 00:47:13 +0700

    tt

---
+++ FE/src/Pages/KhachHang.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Menu from '../ClientComponents/Menu/Menu'
+
+
+
+const KhachHang = () => {
+   
+    return (
+        <div style={{ display: "flex", alignItems: "center" }}>
+       
+        <Menu />
+  
+      
+        <div style={{ marginLeft: "20px" }}>
+          <h1>Khach hang</h1>
+        </div>
+      </div>
+    )
+}
+
+export default KhachHang
