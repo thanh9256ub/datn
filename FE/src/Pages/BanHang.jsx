78ebc8d9573b734f95fe68499d06d10bb9388bb6
tuan45734 <tuan45734>
2025-02-13 01:27:01 +0700

    tt

--- FE/src/Pages/BanHang.jsx
+++ FE/src/Pages/BanHang.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
-import Menu from '../ClientComponents/Menu/Menu'
-
-
+import React from "react";
+import Menu from "../ClientComponents/Menu/Menu";
+import DonHang from "../ClientComponents/DonHang/DonHang";
+import SanPham from "../ClientComponents/SanPham/SanPham";
+import GioHang from "../ClientComponents/GioHang/GioHang";
+import ThanhToan from "../ClientComponents/ThanhToan/ThanhToan";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const BanHang = () => {
-   
-    return (
-        <div style={{ display: "flex", alignItems: "center" }}>
-       
+  return (
+    <div className="d-flex vh-100">
+      {/* Sidebar Menu Fixed */}
+      <div
+        className="left bg-dark text-white p-3  h-100"
+        style={{ height: "100%", width: "20%" }}
+      >
         <Menu />
-  
-      
-        <div style={{ marginLeft: "20px" }}>
-          <h1>Bán hàng</h1>
+      </div>
+
+      {/* Content Section */}
+      <div
+        className="right d-flex flex-grow-1 ms-3 p-3"
+        style={{ marginLeft: "260px", height: "100vh" }}
+      >
+        <div className="Noidung" style={{ overflowY: "auto", height: "100%", width: "100%" }}>
+          <DonHang />
+          <SanPham />
+          <GioHang />
+          <ThanhToan />
         </div>
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default BanHang
+export default BanHang;
