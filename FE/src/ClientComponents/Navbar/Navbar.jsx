51986c128a5214632c59cb8236db37597c836838
thanh9256ub <thanh9256ub@gmail.com>
2025-02-25 10:14:24 +0700

    revert

--- FE/src/ClientComponents/Navbar/Navbar.jsx
+++ FE/src/ClientComponents/Navbar/Navbar.jsx
@@ -15,11 +15,6 @@ const Navbar = () => {
     }
     return (
         <div className='navbar-client'>
-<<<<<<< HEAD
-            <div className='nav-logo'>
-                <img src={logo} alt="" />
-                <p>SHOPPER</p>
-=======
             <div className="nav-left">
                 <div className='nav-logo'>
                     <img src={logo} alt="" />
@@ -36,16 +31,7 @@ const Navbar = () => {
 
                     </ul>
                 </div>
->>>>>>> parent of 032a528 (commit order admin temp p1)
             </div>
-            <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
-            <ul ref={menuRef} className="nav-menu">
-                <li onClick={() => { setMenu("shop") }}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link> {menu === "shop" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("all") }}><Link style={{ textDecoration: 'none' }} to='/all'>All</Link> {menu === "all" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("mens") }}><Link style={{ textDecoration: 'none' }} to='/mens'>Top Sellers</Link> {menu === "mens" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("womens") }}> <Link style={{ textDecoration: 'none' }} to='/womens'>New Product</Link> {menu === "womens" ? <hr /> : <></>}</li>
-                <li onClick={() => { setMenu("kids") }}><Link style={{ textDecoration: 'none' }} to='/kids'>Others</Link> {menu === "kids" ? <hr /> : <></>}</li>
-            </ul>
             <div className="nav-login-cart">
                 <Link to='/login'><button>Login</button></Link>
                 <Link to='/cart'><img src={cart_icon} alt="" /></Link>
